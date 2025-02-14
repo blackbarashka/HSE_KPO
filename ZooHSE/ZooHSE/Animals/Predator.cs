9e8e5ef6f4aef67ca79880b3be9cc1ce406b852b
blackbarashka <144563219+blackbarashka@users.noreply.github.com>
2025-02-14 20:06:00 +0300

    Delete ZooHSE directory

--- ZooHSE/ZooHSE/Animals/Predator.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ZooHSE.Animals
-{
-    public class Predator : Animal
-    {
-        public Predator(int food, int number, string name, bool ishealty, string descrip) : base(food, number, name, ishealty, descrip) { }
-    }
-}
